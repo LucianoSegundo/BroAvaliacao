006b9c68fb4aefa5728f8458619c3bf16b4e9bdb
LucianoSegundo <103046717+LucianoSegundo@users.noreply.github.com>
2024-11-16 21:34:08 -0300

    construção da homepage

---
+++ src/app/DTOs/Estado.ts
@@ -0,0 +1,12 @@
+export class Estado{
+    Nome: string = "";
+    cidades: Cidade[] = [];
+}
+
+export class Cidade{
+
+    Nome: string = "";
+    Latitude: string = "";
+    Longitude: string = "";
+
+}
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, RequiredValidator, Validators } from '@angular/forms';
+import {  MatButtonModule } from '@angular/material/button';
+import {  MatInputModule} from "@angular/material/input";
+import { MatSelectModule } from '@angular/material/select';
+import { Estado, Cidade } from '../DTOs/Estado';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [MatInputModule, ReactiveFormsModule, MatButtonModule, MatSelectModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
 
+  estados: Estado[] =[] ;
+  cidades: Cidade[]= [];
+  
+  formulario = new FormGroup({
+    login: new FormControl("", [Validators.required, Validators.minLength(4)]),
+    senha: new FormControl("", [Validators.required, Validators.minLength(4)])
+  });
+
+  formCadastro = new FormGroup({
+    nome: new FormControl("",[Validators.required, Validators.minLength(4)]),
+    senha: new FormControl("",[Validators.required, Validators.minLength(4)]),
+    confSenha: new FormControl("",[Validators.required, Validators.minLength(4)]),
+    email: new FormControl("",[Validators.required, Validators.minLength(4), Validators.email]),
+    data: new FormControl("",[Validators.required, Validators.minLength(10)]),
+    cidade: new FormControl("",[Validators.required, Validators.minLength(5)]),
+    estado: new FormControl("",[Validators.required, Validators.minLength(5)]),
+    fperfil: new FormControl("")
+  });
+
+ 
+ 
 }
