ce5d77d211b0f629f5872f50a9d72f61efa59b86
LucianoSegundo <103046717+LucianoSegundo@users.noreply.github.com>
2024-11-17 17:17:50 -0300

    adição da recuperação

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
+import { PaginaRecupeComponent } from './pagina-recupe/pagina-recupe.component';
 
 export const routes: Routes = [
-   {path: "", component:HomePageComponent}
+   {path: "", component:HomePageComponent},
+   {path: "recuperar", component:PaginaRecupeComponent}
 ];
