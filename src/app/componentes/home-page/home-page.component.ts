67cc6f1a90d1d62df93b8d35c0b475fa8bc36460
LucianoSegundo <103046717+LucianoSegundo@users.noreply.github.com>
2024-11-21 21:16:52 -0300

    reorganização do projeto e adição de requisições

--- src/app/componentes/home-page/home-page.component.ts
+++ src/app/componentes/home-page/home-page.component.ts
@@ -3,7 +3,12 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import {  MatButtonModule } from '@angular/material/button';
 import {  MatInputModule} from "@angular/material/input";
 import { MatSelectModule } from '@angular/material/select';
-import { Estado, Cidade } from '../DTOs/Estado';
+import { Estado, Cidade } from '../../DTOs/Estado';
+import { ApiService } from '../../servicos/apiService/api.service';
+import { loginRequest } from '../../DTOs/LoginDTO';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { cadastroUsuarioRequest } from '../../DTOs/cadastroDTO';
 
 
 @Component({
@@ -15,6 +20,9 @@ import { Estado, Cidade } from '../DTOs/Estado';
 })
 export class HomePageComponent implements AfterViewInit{
 
+  constructor(private api:ApiService, private router:Router){
+    this.coletarEstados();
+  }
 ngAfterViewInit() {let redi = sessionStorage.getItem("redirecionamento");
   if(redi != null && redi == "login"){
 
@@ -23,7 +31,7 @@ ngAfterViewInit() {let redi = sessionStorage.getItem("redirecionamento");
     sessionStorage.removeItem("redirecionamento");
   }
 }
-
+  mostrarCidade: boolean = true; 
   estados: Estado[] =[] ;
   cidades: Cidade[]= [];
   
@@ -40,10 +48,42 @@ ngAfterViewInit() {let redi = sessionStorage.getItem("redirecionamento");
     data: new FormControl("",[Validators.required, Validators.minLength(10)]),
     cidade: new FormControl("",[Validators.required, Validators.minLength(5)]),
     estado: new FormControl("",[Validators.required, Validators.minLength(5)]),
-    fperfil: new FormControl("")
+    fperfil: new FormControl(null)
   });
 
- 
+  fazerLogin(){
+    
+    if(this.formulario.valid){
+      this.api.LogarUsuario(this.formulario.value as loginRequest ).subscribe({
+        next: (data) =>{
+          sessionStorage.setItem("token",data.accessToken );
+          this.formulario.reset;
+          this.router.navigate(["/paginaPrincipal"]);
+
+        },
+        error: (error:HttpErrorResponse) =>{
+          this.formulario.reset;
+          alert(error.error.message)
+
+        }
+      });
+    }
+  }
+
+  fazerCadastro(){
+    if(this.formCadastro.valid){
+      this.api.cadastrarUsuario(this.formCadastro.value as cadastroUsuarioRequest).subscribe({
+        next: (data) =>{
+          this.formCadastro.reset;
+          this.scrolar("login");
+        },
+        error: (error:HttpErrorResponse) =>{
+          alert(error.error.message);
+        }
+      });
+    }
+  }
+
   scrolar(lugar:string){
     if("login" == lugar){ 
      window.scrollTo({
@@ -67,5 +107,23 @@ ngAfterViewInit() {let redi = sessionStorage.getItem("redirecionamento");
        });
     }
   }
+
+  private coletarEstados(){
+    this.api.requerirEstados().subscribe({
+      next:(data) =>{
+         this.estados = data;
+
+      },
+      error: (error) =>{
+        alert("algo deu errado");
+      }
+      
+    })
+  }
+
+  selecionarEstado(index:number){
+    this.cidades = this.estados.at(index).cidades;
+    this.mostrarCidade = false;
+  };
  
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    urlBase: "" + "/api/v1"
+
+};
