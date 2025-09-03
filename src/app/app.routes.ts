390408a1e416d75082381f947b9361605a40d33b
LucianoSegundo <103046717+LucianoSegundo@users.noreply.github.com>
2025-09-03 08:43:51 -0300

    conclusão do front end feito por Mateus

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,20 @@
 import { Routes } from '@angular/router';
+
+
 import { HomePageComponent } from './componentes/home-page/home-page.component';
-import { PaginaRecupeComponent } from './componentes/pagina-recupe/pagina-recupe.component';
-import { PaginaPrincipalComponent } from './componentes/pagina-principal/pagina-principal.component';
+import { RestauranteListaComponent } from './componentes/restaurante-lista/restaurante-lista.component';
+import { RestauranteDetalhesComponent } from './componentes/restaurante-detalhes/restaurante-detalhes.component';
+import { RestauranteCriarComponent } from './componentes/restaurante-criar/restaurante-criar.component';
 
 export const routes: Routes = [
-   {path: "", component:HomePageComponent},
-   {path: "recuperar", component:PaginaRecupeComponent},
-   {path:"paginaPrincipal", component:PaginaPrincipalComponent}
+  { path: '', component: HomePageComponent },
+  
+
+  { path: 'restaurantes', component: RestauranteListaComponent },
+  
+
+  { path: 'restaurantes/criar', component: RestauranteCriarComponent },
+
+
+  { path: 'restaurante/:id', component: RestauranteDetalhesComponent },
 ];
